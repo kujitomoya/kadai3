56247f6fd0c7b90b9993fea2e07bcbd367e8ba21
EC2 Default User <ec2-user@ip-172-31-17-159.ap-northeast-1.compute.internal>
2022-12-11 14:49:05 +0000

    [user update機能の作成]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,13 +20,23 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice]="You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path
   end
-
+  
   private
   def book_params
     params.require(:book).permit(:title, :body)
